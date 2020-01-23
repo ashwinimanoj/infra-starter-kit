f3543320877c5392a7383d6c7fe5bf098dba7cc3
singsuyash <singsuyash@gmail.com>
2020-01-24 02:54:03 +0530

    Adds endpoint to demo connection with config-server

---
+++ spring-cloud-config-client/src/main/java/com/singsuyash/springcloudconfigclient/rest/ConfigController.java
@@ -0,0 +1,21 @@
+package com.singsuyash.springcloudconfigclient.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${user.role}")
+    private String role;
+
+    @GetMapping(
+            value = "/whoami/{username}",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
+    }
+}
